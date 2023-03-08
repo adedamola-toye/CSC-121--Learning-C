1483f82b64b9f404f4885ef9ffc2a850790a004f
Adedamola Toye <103189139+adedamola-toye@users.noreply.github.com>
2023-03-08 15:48:47 +0100

    Update Prime-Number.c

--- Chapter 3/3.22 - Prime Number/Prime-Number.c
+++ Chapter 3/3.22 - Prime Number/Prime-Number.c
@@ -9,7 +9,7 @@ int main(void){
     int i=2; //set counter to 2
     //counter isn't 1 because prime numbers are divisible by 1 and itself
     int divisibleBy=0; //This counts  the numbers  that an integer can be divided by except one and the number itself
-    //int isPrime;
+    
 
     //Input integer
     printf("Enter a positive integer: ");
@@ -18,19 +18,15 @@ int main(void){
     while (i<num && num >1){ //ie the counter is within range of 2 to (integer inputted -1), If number to be tested is 5 then i is from 2 to 4
         if(num%i==0){ //If integer inputted  is divisible by the counter
             divisibleBy+=1;
-           // isPrime=0; is prime is false
+           
         }
-        /* else{
-            isPrime=1; is prime is true
-        } */
+       
         ++i; //increase counter by 1
     }
     if(divisibleBy==0){ //If the integer inputted is not divisible by any number ranging from 2 to that number
         printf("%d is a prime number", num);
     }
-   /*  if(isPrime=1){
-        printf("%d is a prime number", num);
-    } */
+   
     else{
         printf("%d is not a prime number", num);
     }
